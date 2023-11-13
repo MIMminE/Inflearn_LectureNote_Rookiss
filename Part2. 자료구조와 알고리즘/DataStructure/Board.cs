382bdb9d648220770f5cfe9a5aa806f96834b6d7
MINmin-e <weluv.ww@gmail.com>
2023-11-13 11:31:26 +0900

    2023. 11. 13

--- Part2. 자료구조와 알고리즘/DataStructure/Board.cs
+++ Part2. 자료구조와 알고리즘/DataStructure/Board.cs
@@ -8,14 +8,19 @@ namespace DataStructure
         {
             Empty,
             Wall,
-            player
-        }
+            Dest,
+			Player
+		}
 
         // 2차원 배열로 사용
         public TileType[,] Tile {  get; private set; }
         public int Size { get; private set; }
         private Player _player;
         Random rand = new Random();
+
+        public int DesY { get; private set; }
+        public int DesX { get; private set; }
+        public void SetDestYX(int y, int x) {  DesY = y; DesX = x; Tile[y, x] = TileType.Dest; }
 
         #region Tile 초기화 로직
         public void Init(int size, Player player)
@@ -53,6 +58,12 @@ namespace DataStructure
                         Tile[y, x] = TileType.Wall;
                         continue;
                     }
+                    
+                    if (x == DesX && y == DesY)
+                    {
+						Tile[y, x] = TileType.Dest;
+                        continue;
+					}
                     Tile[y, x] = TileType.Empty;
                 }
             }
@@ -61,7 +72,13 @@ namespace DataStructure
             {
                 for (int x = 1; x < size; x += 2)
                 {
-                    if (x == size - 2 && y == size - 2)
+					if (x == DesX && y == DesY)
+					{
+						Tile[y, x] = TileType.Dest;
+						continue;
+					}
+
+					if (x == size - 2 && y == size - 2)
                         continue;
 
                     if( x == size - 2)
@@ -74,8 +91,9 @@ namespace DataStructure
                         Tile[y, x + 1] = TileType.Empty;
                         continue;
                     }
+		
 
-                    int randValue = rand.Next(0, 2);
+					int randValue = rand.Next(0, 2);
                     if (randValue == 0)
                         Tile[y + 1, x] = TileType.Empty;
                     else
@@ -94,7 +112,7 @@ namespace DataStructure
             {
                 for (int x = 0; x < size; x++)
                 {
-                    if (x % 2 == 0 | y % 2 == 0)
+					if (x % 2 == 0 | y % 2 == 0)
                     {
                         Tile[y, x] = TileType.Wall;
                         continue;
@@ -154,8 +172,9 @@ namespace DataStructure
             {
                 for (int x = 0; x < Size; x++)
                 {
+
                     if(y == _player.PosY && x == _player.PosX)
-                        GetTileColor(TileType.player);
+                        GetTileColor(TileType.Player);
                     else
                         GetTileColor(Tile[y, x]);
                     Console.Write(CIRCLE);
@@ -167,13 +186,11 @@ namespace DataStructure
         public void GetTileColor(TileType tileType)
         {
             if (tileType == TileType.Empty) Console.ForegroundColor = ConsoleColor.Green;
-            else if (tileType == TileType.player) Console.ForegroundColor = ConsoleColor.Blue;
-            else Console.ForegroundColor = ConsoleColor.Red;
+            else if (tileType == TileType.Player) Console.ForegroundColor = ConsoleColor.Blue;
+			else if (tileType == TileType.Dest) Console.ForegroundColor = ConsoleColor.Yellow;
+			else Console.ForegroundColor = ConsoleColor.Red;
         }
         #endregion
-
-
-
 
     }
 }
--- Part2. 자료구조와 알고리즘/DataStructure/Player.cs
+++ Part2. 자료구조와 알고리즘/DataStructure/Player.cs
@@ -2,16 +2,57 @@
 {
     class Player
     {
+        public enum Dir
+        {
+            Up = 0,
+            Left = 1,
+			Down = 2,
+			Right = 3
+        }
+       
         public int PosY { get; private set; }
         public int PosX { get; private set; }
         private Board _board;
-        Random rand = new Random();
+        private int _dir = (int)Dir.Up;
+        private List<int[]> _movePath = new List<int[]>();
+
+		Random rand = new Random();
 
         public void Initialize(int posY, int posX, int destY, int destX, Board board)
         {
             PosY = posY;
             PosX = posX;
             _board = board;
+            _board.SetDestYX(destY, destX);
+
+            int[] frontY = new int[4] { -1, 0, 1, 0 };
+			int[] frontX = new int[4] { 0, -1, 0, 1 };
+            // 목적지에 도착했는지 확인
+			while (PosY != board.DesY || PosX != board.DesX)
+            {
+				// 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
+				if (_board.Tile[PosY + frontY[(_dir - 1 + 4) % 4], PosX + frontX[(_dir - 1 + 4) % 4]] != Board.TileType.Wall) 
+                {
+                    // 오른족 방향으로 90도 회전, 앞으로 전진
+                    _dir = (_dir - 1 + 4) % 4;
+                    PosY = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _movePath.Add(new int[2] { PosY, PosX });
+				}
+                // 2. 현재 바라보는 방향으로 앞으로 갈 수 있는지 확인.
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] != Board.TileType.Wall)
+                {
+					PosY = PosY + frontY[_dir];
+					PosX = PosX + frontX[_dir];
+					_movePath.Add(new int[2] { PosY, PosX });
+				}
+                else
+                {
+					// 왼쪽 방향 90도 회전
+					_dir = (_dir + 1 + 4) % 4;
+				}
+
+            }
         }
 
         const int MOVE_TICK = 100; // 0.1초
@@ -25,10 +66,17 @@
                 RandMove();
             }
         }
-
+        int Move_count = 0;
         private void RandMove()
         {
-            int randValue = rand.Next(0, 4);
+            if(_movePath.Count > Move_count)
+            {
+				PosY = _movePath[Move_count][0];
+				PosX = _movePath[Move_count][1];
+				Move_count++;
+			}
+			/*
+			int randValue = rand.Next(0, 4);
             switch(randValue)
             {
                 case 0: //상
@@ -47,7 +95,7 @@
                     if (_board.Tile[PosY, PosX + 1] == Board.TileType.Empty)
                         PosX += 1;
                     break;
-            }
-        }
+            }*/
+		}
     }
 }
