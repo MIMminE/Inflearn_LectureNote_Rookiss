13a1bf13ee63fbb4ee2924e89ed051beee5dc7c4
너츠 <weluv.ww@gmail.com>
2023-11-13 22:31:41 +0900

    2023. 11. 13

--- Part2. 자료구조와 알고리즘/Section3. 그래프/Graph_LinkedList.cs
+++ Part2. 자료구조와 알고리즘/Section3. 그래프/Graph_LinkedList.cs
@@ -1,39 +1,180 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Section3
 {
-	class Vertax
+    class Vertax
+    {
+        public List<Vertax> edges = new List<Vertax>();
+        public List<Vertax> CreateGraph()
+        {
+            List<Vertax> v = new List<Vertax>(6)
+            {
+                new Vertax(),
+                new Vertax(),
+                new Vertax(),
+                new Vertax(),
+                new Vertax(),
+                new Vertax()
+            };
+
+            v[0].edges.Add(v[1]);
+            v[0].edges.Add(v[3]);
+
+            v[1].edges.Add(v[0]);
+            v[1].edges.Add(v[2]);
+            v[1].edges.Add(v[3]);
+
+            v[3].edges.Add(v[4]);
+
+            v[5].edges.Add(v[4]);
+
+            return v;
+        }
+    }
+
+    class Edge
+    {
+        public int _destVertax;
+        public int _weight;
+        public Edge(int destVertax, int weight)
+        {
+            _destVertax = destVertax;
+            _weight = weight;
+        }
+    }
+
+    class GraphTest
 	{
-		public List<Vertax> edges = new List<Vertax>();
-
-		public List<Vertax> CreateGraph()
+        #region 그래프 이론
+        static public List<int>[] adj1 = new List<int>[6]
 		{
-			List<Vertax> v = new List<Vertax>(6)
+            new List<int> { 1, 3 },
+            new List<int> { 0, 2, 3 },
+            new List<int> { },
+            new List<int> { 4 },
+            new List<int> { },
+            new List<int> { 4 }
+		};
+
+        static public List<Edge>[] adj2 = new List<Edge>[6]
+		{
+			new List<Edge> { new Edge(1, 15), new Edge(3, 35) },
+			new List<Edge> { new Edge(0, 15), new Edge(2, 5), new Edge(3, 10) },
+			new List<Edge> { },
+			new List<Edge> { new Edge(4, 5) },
+			new List<Edge> { },
+			new List<Edge> { new Edge(4, 5) },
+		};
+
+		static public int[,] adj3 = new int[6, 6]{
+			{ 0, 1, 0, 1, 0, 0 },
+			{ 1, 0, 1, 1, 0, 0 },
+            { 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 1, 0 },
+            { 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 1, 0 }
+        };
+
+        static public int[,] adj4 = new int[6, 6]{
+            { -1, 15, -1, 35, -1, -1 },
+            { 15, -1, 05, 10, -1, -1 },
+            { -1, -1, -1, -1, -1, -1 },
+            { -1, -1, -1, -1, 05, -1 },
+            { -1, -1, -1, -1, -1, -1 },
+            { -1, -1, -1, -1, 05, -1 }
+        };
+        #endregion
+
+        #region 그래프 생성 실습 
+
+        static public List<int>[] adjacent1 = new List<int>[6]
+		{
+			new List<int>() { 1, 3 },
+            new List<int>() { 0, 2, 3 },
+            new List<int>() { 1 },
+            new List<int>() { 0, 1, 4 },
+            new List<int>() { 3, 5 },
+            new List<int>() { 4 }
+        };
+
+		static public int[,] adjacent2 = new int[6,6]
+		{
+		    { 0, 1, 0, 1, 0, 0 },
+            { 1, 0, 1, 1, 0, 0 },
+            { 0, 1, 0, 0, 0, 0 },
+            { 1, 1, 0, 1, 0, 0 },
+            { 0, 0, 0, 1, 0, 1 },
+            { 0, 0, 0, 0, 1, 0 }
+        };
+
+		#endregion
+
+		#region 깊이 우선 탐색(DFS)
+		// 1) 우선 now부터 방문하고
+		// 2) now와 연결된 정점들을 하나씩 확인해서, 방문하지 않은 정점을 방문한다.
+		bool[] visited = new bool[6];
+		public void DFS_toList(int now) // 시작 정점
+		{
+            Console.WriteLine(now);
+			visited[now] = true;
+
+			foreach (int next in adjacent1[now])
 			{
-				new Vertax(),
-				new Vertax(),
-				new Vertax(),
-				new Vertax(),
-				new Vertax(),
-				new Vertax()
-			};
+				if (!visited[next])
+                    DFS_toList(next);
+            }
+        }
 
-			v[0].edges.Add(v[1]);
-			v[0].edges.Add(v[3]);
+		public void DFS_toArray(int now)
+		{
+            Console.WriteLine(now);
+			visited[now] = true;
 
-			v[1].edges.Add(v[0]);
-			v[1].edges.Add(v[2]);
-			v[1].edges.Add(v[3]);
+			for(int next = 0; next < adjacent2.GetLength(0); next++)
+			{
+				if (adjacent2[now, next] == 0) continue;
+				if (!visited[next])
+                    DFS_toArray(next);
+			}
+        }
 
-			v[3].edges.Add(v[4]);
+		public void SearchAll()
+		{
+			for (int i = 0; i < adjacent1.Length - 1; i++)
+			{
+                if (!visited[i])
+				{
+					DFS_toList(i);
+                }
+            }
+        }
+        #endregion
 
-			v[5].edges.Add(v[4]);
+        bool[] found = new bool[6];
+        public void BFS(int start)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
 
-			return v;
-		}
-	}
+            while(q.Count > 0)
+            {
+                int now = q.Dequeue();
+                Console.WriteLine(now);
+                foreach (int next in adjacent1[now])
+                {
+                    if (!found[next])
+                    {
+                        q.Enqueue(next);
+                        found[next] = true;
+                    }
+                }
+            }
+        }
+    }
 }
--- Part2. 자료구조와 알고리즘/Section3. 그래프/Program.cs
+++ Part2. 자료구조와 알고리즘/Section3. 그래프/Program.cs
@@ -7,8 +7,8 @@
 	{
 		static void Main(string[] args)
 		{
-			#region 스택과 큐
-			/*
+            #region 스택과 큐
+            /*
 			Stack<int> stack = new Stack<int>();
 
 			stack.Push(101);
@@ -29,7 +29,7 @@
 			int e = queue.Peek();
 			*/
 
-			/*  기존의 linkedList 등의 자료형을 사용하면 충분히 가능한 기능들을 왜 사용할까?
+            /*  기존의 linkedList 등의 자료형을 사용하면 충분히 가능한 기능들을 왜 사용할까?
 			 *  추상적으로 사용하기 좋기 때문이다. (예: 스택 구조로 만들게요, 큐 구조로 만들게요)
 			 *  실제로 Stack 구현에 있어 List 자료형은 좋은 선택이다. 시간 복잡도의 기준으로 생각해보자.
 			 *  반대로 Queue 구현에 있어 List 자료형은 최악의 선택이다. 보통 순환 버퍼라는 것을 이용하여 구현되는 편이다.
@@ -38,13 +38,10 @@
 			 *  UI 환경에서 팝업창이 여러 개 뜰때, 마지막에 뜬 창이 먼저 꺼져야 한다. (스택 구조)
 			 *  네트워크 패킷을 처리할 때 수많은 유저들의 처리를 먼저 온 사람의 패킷 먼저 처리한다. (큐 구조)
 			 */
-			#endregion
-			Vertax v = new Vertax();
-			List<Vertax> list_v = v.CreateGraph();
-			foreach(var e in list_v)
-			{
-                Console.WriteLine(e);
-            }
+            #endregion
+
+            GraphTest graphTest = new GraphTest();
+            graphTest.BFS(0);
 		}
 	}
 }
